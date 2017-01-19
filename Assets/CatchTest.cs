d67da6feab0cac9c5e616ff2ef761e6e16db002d
m.roppy <roppy@hotmail.co.jp>
2017-01-19 14:54:02 +0900

    四角形範囲で受け取る処理ほぼ完成

---
+++ Assets/CatchTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CatchTest : MonoBehaviour 
+{
+	[SerializeField]
+	List<Transform> points;
+
+	[SerializeField]
+	Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( target == null )
+		{
+			return;
+		}
+
+
+		if( SquareDetector.IsInside( points[0].position, points[1].position, points[2].position, points[3].position, target.position) )
+		{
+			target.GetComponent<Rigidbody>().AddForce(Vector3.up * 300);
+		}
+	}
+}
