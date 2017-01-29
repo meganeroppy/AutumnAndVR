3b8015a4a494f28bfa0a1e1660ba61838db3f6d6
A11DC133 <roppy@hotmail.co.jp>
2017-01-29 09:40:13 +0900

    飾り置いた

---
+++ Assets/Planet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+
+	public float rotSpeed;
+	public Vector3 rotDigree;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (rotDigree * rotSpeed * Time.deltaTime );
+	}
+}
